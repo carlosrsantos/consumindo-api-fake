ea48977ae466de20da1d6b5f0febe6ada2d524dc
Carlos Rodrigues <crodrigsantos@gmail.com>
2021-05-17 23:49:53 -0300

    configurando Mirage e Axios e adicionando Modal

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,13 +1,24 @@
-import { Container, Content } from "./styles";
-import logoImg from '../../assets/logo.svg'
-export function Header() {
+import { Container, Content } from './styles';
+import logoImg from '../../assets/logo.svg';
+
+interface HeaderProps {
+  onOpenNewTransactionModal: () => void;
+}
+
+export function Header({onOpenNewTransactionModal}: HeaderProps) {
+  
   return (
     <Container>
       <Content>
         <img src={ logoImg } alt="dt money" />
-        <button type="button">
+        <button 
+          type="button" 
+          onClick={onOpenNewTransactionModal}
+        >
           Nova transação
         </button>
+
+       
       </Content>
     </Container>
   )
